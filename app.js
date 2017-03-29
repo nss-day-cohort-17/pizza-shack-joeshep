f638eb6190203e6ae26f37c65a2020491cda64e3
Michael Watson <m.watson.tn@gmail.com>
2017-03-29 15:02:36 -0500

    server functions and index home view works, deps linked

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+//pug config
+app.set('view engine', 'pug');
 
+//middlewares
+app.use(express.static('public'));
+
+app.get('/', (req, res, next) => {
+	res.render('index');
+})
 
 
 
@@ -20,5 +28,5 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log('Listening on port ${port}')
+	console.log(`Listening on port ${port}`)
 })
