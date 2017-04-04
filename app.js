3f7afeffdc1a3ea3a536782341924d77ef3749cd
Michael Watson <m.watson.tn@gmail.com>
2017-04-04 15:14:39 -0500

    updating routes

--- app.js
+++ app.js
@@ -4,40 +4,42 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const routes = require('./routes/')
+
 //pug config
 app.set('view engine', 'pug');
 
+//sets a variable that the whole app can use
+app.locals.company = 'Pizza Shack';
+app.locals.body = {};
+app.locals.body.magic = ''
+
 //middlewares
 app.use(express.static('public'));
+app.use(routes);
 
-app.get('/', (req, res, next) => {
-	res.render('index');
-})
-
-app.get('/about', (req, res, next) => {
-	res.render('about')
-})
 
 app.get('/contact', (req, res, next) => {
-	res.render('contact')
+	res.render('contact', {page: 'Contact'})
 })
 
 app.get('/order', (req, res, next) => {
-	res.render('order')
+	res.render('order', {page: 'Order'})
 })
 
 app.get('/login', (req, res, next) => {
-	res.render('login')
+	res.render('login', {page: 'Login'})
 })
 
+app.get('/register', (req, res, next) => {
+	res.render('/register', {page: 'Register'})
+})
 
+app.use((req, res) => {
+	res.render('404');
+})
 
-
-
-
-
-
-
+//***************end of middlewares**************
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { Router } = require('express');
+const router = Router();
+
+//public routes
+router.use(require('./about'));
+// router.use(require('./contact'));
+// router.use(require('./login'));
+// router.use(require('./register'));
+router.use(require('./root'));
+
+//login guard middleware -> return to home if not registered
+// router.use((req, res, next) => {
+// 	if (req.isAuthenticated()) {
+// 		next();
+// 	} else {
+// 		res.redirect('/login')
+// 	}
+// });
+
+//private routes
+// router.use(require('./logout'))
+// router.use(require('./order'))
+
+module.exports = router;
